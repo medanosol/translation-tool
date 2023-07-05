f68e171676d2d106c55ec2c25b2b80d24ea73a7d
David Díaz <daviddiazh97@gmail.com>
2023-07-05 10:30:33 +0100

    adaptions

--- src/App.tsx
+++ src/App.tsx
@@ -12,8 +12,13 @@ const queryClient = new QueryClient({
 function App() {
   return (
     <div className="container flex flex-col items-center justify-center w-screen min-h-screen px-20 py-10 mx-auto">
-      <header className="mb-4 text-2xl font-bold text-center text-sky-600">
-        medsol-i18n
+      <header className="fixed top-0 flex flex-col gap-2 p-4">
+        <span className="text-6xl font-bold text-center text-sky-600">
+          medsol-i18n
+        </span>
+        <span className="text-2xl font-bold text-center text-sky-600">
+          A simple tool to translate your i18n files
+        </span>
       </header>
       <main className="w-full">
         <DeeplContextProvider>
