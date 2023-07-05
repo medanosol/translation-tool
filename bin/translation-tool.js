9e25ff8afeeea2f6ba8174ffbae432195376d8b8
Raúl Rodríguez Torres <raul@medanosol.es>
2023-07-05 12:50:34 +0100

    Shell true for spawn child process (window users)

--- bin/translation-tool.js
+++ bin/translation-tool.js
@@ -1,24 +1,25 @@
 #!/usr/bin/env node
 
-import { spawn } from "child_process";
-import { dirname, resolve } from "path";
-import { fileURLToPath } from "url";
+import { spawn } from 'child_process';
+import { dirname, resolve } from 'path';
+import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 // get the port from the command line or use the default port 3000
-const portIndex = process.argv.indexOf("--port");
+const portIndex = process.argv.indexOf('--port');
 const port = portIndex !== -1 ? process.argv[portIndex + 1] : 3000;
 
 const serveProcess = spawn(
-  "serve",
-  [resolve(__dirname, "../dist"), "-l", port],
+  'serve',
+  [resolve(__dirname, '../dist'), '-l', port],
   {
-    stdio: "inherit",
+    stdio: 'inherit',
+    shell: true,
   }
 );
 
-serveProcess.on("exit", (code) => {
+serveProcess.on('exit', (code) => {
   process.exit(code);
 });
