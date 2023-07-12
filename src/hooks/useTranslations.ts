0f94cfe4f78f170e1a98aedc2f6f39087b5de12f
David Díaz <daviddiazh97@gmail.com>
2023-07-12 13:57:57 +0100

    Handle API requests properly

--- src/hooks/useTranslations.ts
+++ src/hooks/useTranslations.ts
@@ -1,4 +1,5 @@
 import { UseMutateAsyncFunction, useMutation } from "react-query";
+import { toast } from "react-toastify";
 import { useDeeplContext } from "../components/deeplContext/DeeplContext";
 
 type IUseTranslations = ({
@@ -19,20 +20,24 @@ export const useTranslations: IUseTranslations = ({
   const DEEPL_KEY = deeplApiKey;
 
   async function translateText(text: string) {
-    const urlDeepL =
-      "https://api-free.deepl.com/v2/translate?auth_key=" +
-      DEEPL_KEY +
-      "&text=" +
-      text +
-      "&source_lang=" +
-      sourceLang +
-      "&target_lang=" +
-      lang +
-      "&preserve_formatting=1";
-    const responseDeepL = await fetch(urlDeepL);
-    const dataDeepL = await responseDeepL.json();
-    const translatedText = dataDeepL.translations[0].text;
-    return translatedText;
+    try {
+      const urlDeepL =
+        "https://api-free.deepl.com/v2/translate?auth_key=" +
+        DEEPL_KEY +
+        "&text=" +
+        text +
+        "&source_lang=" +
+        sourceLang +
+        "&target_lang=" +
+        lang +
+        "&preserve_formatting=1";
+      const responseDeepL = await fetch(urlDeepL);
+      const dataDeepL = await responseDeepL.json();
+      const translatedText = dataDeepL.translations[0].text;
+      return translatedText;
+    } catch (error) {
+      toast.error("Error while translating text");
+    }
   }
 
   const { mutateAsync } = useMutation(translateText);
